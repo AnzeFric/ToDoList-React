c7e8fe438e8318d118029c465354e6fe55184ae3
AnzeFric <120261988+AnzeFric@users.noreply.github.com>
2024-07-02 18:35:27 +0200

    Error fixes

--- src/components/CustomCheckBox.js
+++ src/components/CustomCheckBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Checkbox } from '@carbon/react';
 
-const CustomCheckbox = ({ labelText, id, checked, onChange }) => {
+const CustomCheckbox = ({ labelText, checked, onChange }) => {
   return (
     <div className="custom-checkbox">
       <Checkbox
-        id={id}
+        id=""
         className="todo-item-checkbox"
         labelText={<h5>{labelText}</h5>}
         checked={checked}
